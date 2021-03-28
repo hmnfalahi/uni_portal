5a59d1033f18789915e9f94f7a34760005ea2eb5
hmnfalahi@gmail.com <hmnfalahi@gmail.com>
2021-03-28 12:52:48 +0430

    Adding students app

---
+++ students/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from members.models import User
+
+
+GRADES_CHOICES = (
+    ('AS', 'Associate'),
+    ('BC', 'Bachelor'),
+    ('MS', 'Master'),
+    ('DR', 'Doctorate'),
+)
+
+
+STATUS_CHOICES = (
+    ('ST', 'Studying'),
+    ('AS', 'Graduated'),
+    ('BC', 'Fired'),
+)
+
+
+class Student(User):
+    student_code = models.CharField(max_length=14)
+    incoming_semester = models.CharField(max_length=3)
+    grade = models.CharField(
+        max_length=2,
+        choices=GRADES_CHOICES,
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+    )
