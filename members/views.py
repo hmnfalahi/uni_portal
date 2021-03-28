53e694652cbc4f3704a2632b042f405f3b7bf26b
hmnfalahi@gmail.com <hmnfalahi@gmail.com>
2021-03-28 13:18:53 +0430

    Adding members list view

--- members/views.py
+++ members/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+def members_list(request):
+    members = User.objects.all()
+    context = {
+        'members': members,
+    }
+
+    return render(request, 'members_list.html', context)
--- uni_portal/settings.py
+++ uni_portal/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     # installed apps
     'members.apps.MembersConfig',
     'students.apps.StudentsConfig',
+    'teachers.apps.TeachersConfig',
 ]
 
 MIDDLEWARE = [
